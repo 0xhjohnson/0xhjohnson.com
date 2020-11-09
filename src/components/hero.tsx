9de1bd5c81a978698f17aac996049e51abbf38c6
0xhjohnson <0xhjohnson@protonmail.com>
2020-11-08 21:28:12 -0500

    Added hero to homepage

---
+++ src/components/hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div>
+      <h2 className="text-4xl tracking-light font-extrabold text-gray-900">
+        Recent publications
+      </h2>
+      <p className="mt-4 text-xl text-gray-500 pb-12 border-b-2 border-gray-100">
+        Personal blog by Hunter Johnson aka 0xhjohnson
+      </p>
+    </div>
+  );
+}
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 
 import Layout from '../components/layout';
+import Hero from '../components/hero';
 
 function Home() {
   return (
@@ -9,6 +10,11 @@ function Home() {
         <title>0xhjohnson — A blog by Hunter Johnson</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main>
+        <div className="bg-white mt-24 px-4 max-w-2xl mx-auto">
+          <Hero />
+        </div>
+      </main>
     </Layout>
   )
 }
