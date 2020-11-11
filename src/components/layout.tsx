157a2864775054535cfe0a0b6d73525460cb19ad
0xhjohnson <0xhjohnson@protonmail.com>
2020-11-10 20:04:36 -0500

    Going to use type over interface when possible

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Footer from './footer';
 
-interface LayoutProps {
+type LayoutProps = {
   children: React.ReactNode;
-}
+};
 
 function Layout({ children }: LayoutProps) {
   return (
