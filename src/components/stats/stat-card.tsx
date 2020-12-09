fc363ee2b13446a77e8c58a67699254f3858a2ae
0xhjohnson <0xhjohnson@protonmail.com>
2020-12-09 00:29:31 -0500

    Display emdash when data is incoming

--- src/components/stats/stat-card.tsx
+++ src/components/stats/stat-card.tsx
@@ -12,7 +12,7 @@ function StatCard({ title, stat, link }: Props) {
       <a className="block p-4 border border-gray-200 rounded-md hover:border-gray-100 shadow-md hover:shadow-lg dark:border-gray-600 dark:hover:border-gray-700 transition">
         <p className="text-sm text-gray-500 font-medium">{title}</p>
         <p className="mt-1 text-gray-900 dark:text-gray-100 font-semibold text-2xl">
-          {stat}
+          {stat || '\u2014'}
         </p>
       </a>
     </Link>
