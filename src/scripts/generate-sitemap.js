e185064673da620eb53566452eab417296a1a5b5
0xhjohnson <0xhjohnson@protonmail.com>
2020-12-14 20:31:55 -0500

    Correct the incorrect namespace error from google

--- src/scripts/generate-sitemap.js
+++ src/scripts/generate-sitemap.js
@@ -16,7 +16,7 @@ const prettier = require('prettier');
 
   const sitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xlmns="http://www.sitemap.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${pages
         .map((page) => {
           const path = page
