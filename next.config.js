42d88cb66d016959939dd0faabd969d976c252c2
0xhjohnson <0xhjohnson@protonmail.com>
2021-07-04 00:38:13 -0400

    added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
+
+module.exports = withBundleAnalyzer({
   webpack: (config, { isServer }) => {
     if (isServer) {
       require('./src/scripts/generate-sitemap');
@@ -9,4 +13,4 @@ module.exports = {
   images: {
     domains: ['i.scdn.co']
   }
-};
+});
